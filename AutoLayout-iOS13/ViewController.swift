cb291473885a04f4497eb8a4722fa4dc6e4aa2cc
Leon J Mac <leonjmac@protonmail.ch>
2020-04-15 00:44:58 +0100

    Updated with AutoLayout constraints, animations and response to motion controls.

--- AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13/ViewController.swift
@@ -12,15 +12,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        rollButton.isEnabled = false
         
-        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-        
+        diceImageView1.animateShake(completion: { (didComplete: Bool) in
+            self.rollButton.isEnabled = didComplete
+        })
+        diceImageView2.animateShake(completion: { (didComplete: Bool) in
+            self.rollButton.isEnabled = didComplete
+        })
         diceImageView1.image = allDice[Int.random(in: 0...5)]
-        diceImageView2.image = allDice[Int.random(in: 0...5)]
-        
+        diceImageView2.image = self.allDice[Int.random(in: 0...5)]
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if (motion == .motionShake) {
+            self.rollButtonPressed(motion)
+        }
     }
     
 }
 
+extension UIView {
+    func animateShake(withOption option:UIView.AnimationOptions = .curveEaseInOut, completion: ((Bool) -> Void)?) {
+        self.transform = CGAffineTransform(translationX: CGFloat(Double.random(in: -25...25)), y: CGFloat(Double.random(in: -25...25)))
+        UIView.animate(withDuration: 0.54, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: completion)
+    }
+}
